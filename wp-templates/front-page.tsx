491295cc48bdbb4e1887968742a7a1cada6b643b
Huey Kirby <terrancekirby93@gmail.com>
2021-08-11 14:28:31 -0500

    updated site hero title

--- wp-templates/front-page.tsx
+++ wp-templates/front-page.tsx
@@ -25,7 +25,7 @@ export default function FrontPage(props: any): JSX.Element {
       <Header title={settings?.title} description={settings?.description} />
       <main className="content">
         <Hero
-          title="Get Started with Headless"
+          title="updating text can be hard"
           buttonText="Developer Docs"
           buttonURL="https://developers.wpengine.com/"
           button2Text="Headless on GitHub"
